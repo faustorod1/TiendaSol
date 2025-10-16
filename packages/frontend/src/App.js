b05138200602840020f70d2d4564d099feefb895
faustorod1 <faurodriguez@frba.utn.edu.ar>
2025-10-16 15:03:59 -0300

    barra de navegacion

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/mainPage/Header";
 import NavBar from "./components/mainPage/NavBar";
+import ProductSearchBar from './components/ProductSearchBar/ProductSearchBar';
+
 import "./App.css";
 
 function App() {
@@ -20,6 +22,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <NavBar></NavBar>
+      <ProductSearchBar></ProductSearchBar>
     </div>
   );
 }
