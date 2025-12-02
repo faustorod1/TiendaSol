e3a6d4fec530075dfc8a372459c7eb0c37c7185b
Manuel Rafael <mrafael@frba.utn.edu.ar>
2025-12-02 13:07:06 -0300

    Actualizada conexión con la DB

--- packages/backend/src/config/database.js
+++ packages/backend/src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export class MongoDBClient {
     static async connect() {
         try {
-            const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_DB_NAME}`);
+            const conn = await mongoose.connect(`${process.env.MONGODB_URI}`);
 
             console.log(`MongoDB is connected: ${conn.connection.host}`);
         } catch (error) {
