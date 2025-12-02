94f3627061e2a9f6c7aaa41ea8a786ce5ea395f9
Juan Tarducci <jtarducci@frba.utn.edu.ar>
2025-12-02 18:08:19 -0300

    ultimo commit
    posta

--- packages/frontend/src/service/usuarioService.js
+++ packages/frontend/src/service/usuarioService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:8000'
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000'
 const API_URL = `${BASE_URL}/usuarios`;
 
 export async function registerUser(userData) {
