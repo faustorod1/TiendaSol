187d7be403bce091b1774f576bc1e66c22d1c459
Juan Tarducci <jtarducci@frba.utn.edu.ar>
2025-12-02 17:46:29 -0300

    cambio api url - frontend

--- packages/frontend/src/service/pedidoService.js
+++ packages/frontend/src/service/pedidoService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+const BASE_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:8000'
 const API_URL = `${BASE_URL}/pedidos`;
 
 export async function createOrder(orderData) {
